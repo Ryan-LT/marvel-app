b08f4dc05cff98d5d60876f4421947501c75f03c
ThinhNu <thinh.le.sdev@gmail.com>
2019-04-27 10:40:06 +0800

    Update index.js
    Update https call to avoid unsafescripts

--- src/apiHelper/index.js
+++ src/apiHelper/index.js
@@ -3,7 +3,7 @@ import CryptoJS from 'crypto-js'
 
 const PRIV_KEY = "0577ee6a17331d9bf7b3ce2fee226a7664c681c7";
 const PUBLIC_KEY = "b1a2971390cc3f558035272fe497270f";
-const API_URL = 'http://gateway.marvel.com:80/v1/public/characters';
+const API_URL = 'https://gateway.marvel.com/v1/public/characters';
 
 export const getListCharacter = (rowsPerPage, pageNum) => {
     const ts = new Date().getTime();
